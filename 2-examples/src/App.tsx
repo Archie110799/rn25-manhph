92f56b739d59d522219accdf7611b0acca39f907
manhph <manhph@vnext.vn>
2022-10-01 02:10:43 +0700

    add example

---
+++ 2-examples/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import JSX from "./2-examples/JSX";
+import Component from "./2-examples/Component";
+import State from "./2-examples/State";
+
+function App() {
+  return (
+    <>
+      {/* <JSX /> */}
+      <Component />
+      {/* <State /> */}
+    </>
+  );
+}
+
+export default App;
